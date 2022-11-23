28c3fb07c43d3ced4587ae10db1ee5d08c7ae670
IvanVelazquezz98 <Velazquez.ivan.e98@gmail.com>
2022-11-23 18:14:06 -0300

    start App

---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,22 @@
+import react from 'react'
+import { Link  } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { useDispatch , useSelector  } from 'react-redux';
+
+
+export default function Home() {
+    const dispatch = useDispatch() 
+
+    useEffect(() => {
+
+    },[])
+
+    return(
+        <div >
+            
+        </div>
+    
+        )
+    
+
+}
---
+++ src/Redux/Reducer/index.js
@@ -0,0 +1,20 @@
+const initialState = {
+    detailUser: []
+  }
+  
+  
+  
+  
+  export default function rootReducer(state = initialState, action) {
+    switch (action.type) {
+      case "GET_USER":
+        return {
+          ...state,
+          detailUser: action.payload
+        }
+  
+  
+      default: return state
+    }
+  
+  }
