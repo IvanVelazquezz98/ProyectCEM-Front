d9cbf1e4ae07d5cb9572ab439a4772b908e2f08b
IvanVelazquezz98 <Velazquez.ivan.e98@gmail.com>
2022-11-25 12:00:06 -0300

    se movio el creador de estudios al contador de estudios

--- src/Components/Pages/Contador/Contador.jsx
+++ src/Components/Pages/Contador/Contador.jsx
@@ -1,13 +1,38 @@
 
 import styles from "./Contador.module.css"
+import React, { useState, useEffect } from "react";
+import iconMas from '../../../Assets/iconMas.svg'
+import ModalCreateStudy from "./ModalCreateStudy";
+
 export default function Contador ({user}){
 
+    const [ createStudy , setCreateStudy] = useState(false)
 
+    const [input, setInput] = useState({
+        name : "",
+        method : "",
+        reference : "",
+        priority : "",
+        files : "",
+        notes : "",
+        userId : user?.id,
+        sedeId : user?.sede?.id
+    })
+  
+
+    const handleCreateStudy = () => {
+        setCreateStudy(true)
+
+    }
     
     return (
-        <div className={styles.flex} >
-        <div className={styles.info}>Tu contador de estudios {user.name}</div>
-        
+        <div className={styles.flex}>
+        <div className={styles.divCreateStudy}>
+            <img onClick={(e) => handleCreateStudy(e)} className={styles.iconMas} src={iconMas} />
         </div>
+        { createStudy ? <ModalCreateStudy user= {user} closeModal={setCreateStudy}/> : null}
+
+        <div>Lista de estudios creados va abajo</div>
+    </div>
     )
 }
